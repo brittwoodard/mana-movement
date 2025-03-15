27f3d4bb723075977b2c54b188642ac2202d1558
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2025-03-15 14:46:35 +0100

    testimonial structure

---
+++ main.js
@@ -0,0 +1,23 @@
+// Slick
+$('.testimonial-container').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 500,
+    arrows: false,
+    accessibility: true,
+    dots: true,
+    fade: true,
+    infinite: true,
+    pauseOnHover: true,
+});
+
+$('.text-container').slick({
+    autoplay: true,
+    speed: 1000,
+    arrows: true,
+    accessibility: true,
+    dots: true,
+    fade: true,
+    infinite: true,
+    pauseOnHover: false,
+});
